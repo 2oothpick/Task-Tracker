a98cf9bac7728da2e5faa3cb8c01a9be7c810fa4
2oothpick <kobbyboateng14@gmail.com>
2025-06-14 21:07:13 +0000

    Add file storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,37 @@
 
 from uuid import uuid4
 from datetime import datetime
+import models
+
 
 class BaseModel:
 	"""
 	BaseModel for tasks
 	"""
+	id = 0
 	
-	def __init__(self, description, status):
-		self.id = uuid4()
+	def __init__(self, description, status='todo'):
+		BaseModel.id += 1
+		self.id = BaseModel.id
 		self.description = description
 		self.status = status
 		self.created_at = datetime.now()
 		self.updated_at = datetime.now()
 	
+	
 	def __str__(self):
 		return f"id: {self.id} — description: {self.description} — status: {self.status}"
 
 	def to_dict(self):
-		return self.__dict__
+		return_dict = self.__dict__.copy()
+		for k, v in return_dict.items():
+			if isinstance(v, datetime):
+				return_dict[k] = v.isoformat()
+		return return_dict
+		
+	def save(self):
+		obj = self.to_dict()
+		models.storage.new(obj)
+		models.storage.save()
+
+
---
+++ models/storage_engine/file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+
+class FileStorage:
+	
+	__file_name = 'file.json'
+	__objs = None
+	
+	def save(self):
+		""" serializes self.__objs to self.__file_name """
+		with open(self.__file_name, 'w') as file:
+			json.dump(self.__objs,file)
+	
+	def new(self, obj):
+		""" adds obj to self.__objs """
+		self.__objs.append(obj)
+	
+	def reload(self):
+		"""
+		deserializes objects in self.__file_name
+		and returns them to self.__objs
+		"""
+		with open(self.__file_name, 'r') as file:
+			self.__objs = json.load(file)
+	
+	def all(self):
+		""" returns all objects stored in the file """
+		return self.__objs
